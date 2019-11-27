583899e54f6bfc932fe8970cb785388f8c81d46e
mortimer <mortimervonchappuis@protonmail.com>
2019-11-27 02:28:43 +0100

    Aufgabe 2/3

--- Kryptologiepraktikum 2.py
+++ Kryptologiepraktikum 2.py
@@ -1,11 +1,12 @@
 from random import randrange
+from matplotlib import pyplot as plt
 
 
 def miller_rabin(n, it=1000):
     return all(witness(n, randrange(1, n)) for _ in range(it))
 
 
-def witness(n, a, status=False):
+def witness(n, a):
     k = n-1
     x = pow(a, k, n) 
     while x == 1 and k % 2 == 0:
@@ -16,13 +17,27 @@ def witness(n, a, status=False):
 
 
 def next_prime(n, it=1000):
-    n += (1&n)^1
+    n += (1&n)^1 # macht n ungerade
     while not miller_rabin(n, it):
         n += 2
     return n
 
-test = [(17, 5, 17), (32, 5, 37), (pow(10, 100), 10, pow(10, 100) + 267)]
 
-if all(next_prime(a, b) == c for a, b, c in test):
-    print("NextPrime Test Bestanden")
+def number_witness(n):
+    return sum(not witness(n, a) for a in range(1, n))
 
+
+def mean_distance(n, anz, it=1000):
+    distance = lambda x: next_prime(x, it) - x
+    return sum(distance(randrange(n))  for _ in range(anz))/anz
+
+
+test_A = [(17, 5, 17), (32, 5, 37), (pow(10, 100), 10, pow(10, 100) + 267)]
+
+if all(next_prime(a, b) == c for a, b, c in test_A):
+    print("Next-Prime-Test Bestanden")
+
+test_B = [(9, 6), (325, 306)]
+
+if all(number_witness(a) == b for a, b in test_B):
+    print("Number-Witness-Test Bestanden")
